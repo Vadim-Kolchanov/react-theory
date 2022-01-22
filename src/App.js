01a62dd4b9c1ae92550be399df60e0f933acde13
Vadim Kolchanov <ikolchanov14@gmail.com>
2022-01-22 21:25:15 +0500

    Раздел 2: JSX-синтаксис. inline-стили

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World, motherfuckers!</h1>
-    </div>
-  );
+class App extends Component {
+    render() {
+        const divStyle = {
+            // 'text-align' не предпочтительно использовать. Лучше в стиле верблюда textAlign
+            textAlign: 'center'
+        };
+
+        return (
+            <div className="App" style={divStyle}>
+                <h1 style={{color: 'blue', fontSize: '20px'}}>Hello World, motherfuckers!</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
