17d26b1ea2ea3f62c4870c790d307a86ac424901
Vadim Kolchanov <ikolchanov14@gmail.com>
2022-01-25 23:20:39 +0500

    Раздел 3: Основы React. Создание state

--- src/App.js
+++ src/App.js
@@ -3,25 +3,31 @@ import React, {Component} from "react";
 import Car from './Car/Car';
 
 class App extends Component {
+
+    state = {
+        cars: [
+            {name: 'BMW', year: 2019},
+            {name: 'Audi', year: 2016},
+            {name: 'Mazda', year: 2010}
+        ],
+        pageTitle: 'Hello World, motherfuckers!'
+    };
+
     render() {
         const divStyle = {
             // 'text-align' не предпочтительно использовать. Лучше в стиле верблюда textAlign
             textAlign: 'center'
         };
 
+        const cars = this.state.cars;
+
         return (
             <div className="App" style={divStyle}>
-                <h1>Hello World, motherfuckers!</h1>
+                <h1>{this.state.pageTitle}</h1>
 
-                <Car name={'BMW'} year={2018}>
-                    <p style={{color: 'blue'}}>COLOR</p>
-                </Car>
-
-                <Car name="Audi" year={2016}>
-                    <p style={{color: 'red'}}>COLOR</p>
-                </Car>
-
-                <Car name={'Mazda'} year={2010} children={5} />
+                <Car name={cars[0].name} year={cars[0].year}/>
+                <Car name={cars[1].name} year={cars[1].year}/>
+                <Car name={cars[2].name} year={cars[2].year}/>
             </div>
         );
     }
