fcbcdf2ca8c5ad48e016f0a61ebf49cb495a8314
Vadim Kolchanov <ikolchanov14@gmail.com>
2022-01-29 21:00:11 +0500

    Раздел 4: Стилизация компонентов. Динамические классы

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,14 +1,34 @@
 import React from "react";
-import './Car.css'
+import './Car.css';
+
+function inputClassName(props) {
+    const inputClasses = ['input'];
+
+    if (props.name !== '') {
+        inputClasses.push('green');
+    } else {
+        inputClasses.push('red');
+    }
+
+    if (props.name.length > 4) {
+        inputClasses.push('bold');
+    }
+
+    return inputClasses.join(' ');
+}
 
 export default props => (
     <div className="Car">
         <h3>Car name: {props.name}</h3>
         <p>Year: <strong>{props.year}</strong></p>
-        <input type="text" onChange={props.onChangeName} value={props.name} />
+        <input
+            className={inputClassName(props)}
+            type="text"
+            onChange={props.onChangeName} value={props.name}
+        />
         <button onClick={props.onDelete}>Delete</button>
     </div>
-)
+);
 
 //Вывод динамических данных
 // export default () => (
