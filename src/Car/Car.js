129341c5ba0702bad88fd2bb91d316324b520ec5
Vadim Kolchanov <ikolchanov14@gmail.com>
2022-02-01 21:48:32 +0500

    Раздел 4: Стилизация компонентов. Radium

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Radium from 'radium';
 import './Car.css';
 
 function inputClassName(props) {
@@ -17,18 +18,31 @@ function inputClassName(props) {
     return inputClasses.join(' ');
 }
 
-export default props => (
-    <div className="Car">
-        <h3>Car name: {props.name}</h3>
-        <p>Year: <strong>{props.year}</strong></p>
-        <input
-            className={inputClassName(props)}
-            type="text"
-            onChange={props.onChangeName} value={props.name}
-        />
-        <button onClick={props.onDelete}>Delete</button>
-    </div>
-);
+const Car = props => {
+    const style = {
+        border: '1px solid #ccc',
+        boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
+        ':hover': {
+            border: '1px solid #aaa',
+            boxShadow: '0 4px 14px 0 rgba(0, 0, 0, .25)',
+            cursor: 'pointer'
+        }
+    };
+
+    return (<div className="Car" style={style}>
+            <h3>Car name: {props.name}</h3>
+            <p>Year: <strong>{props.year}</strong></p>
+            <input
+                className={inputClassName(props)}
+                type="text"
+                onChange={props.onChangeName} value={props.name}
+            />
+            <button onClick={props.onDelete}>Delete</button>
+        </div>
+    );
+};
+
+export default Radium(Car);
 
 //Вывод динамических данных
 // export default () => (
