981567476b60197f4a49138f72fe1a1adc956ff9
Vadim Kolchanov <ikolchanov14@gmail.com>
2022-02-17 21:18:35 +0500

    Раздел 6: Практика. Компоненты.  Кнопка переключения меню

--- react-quiz/src/hoc/Layout/Layout.js
+++ react-quiz/src/hoc/Layout/Layout.js
@@ -1,10 +1,30 @@
 import React, {Component} from "react";
 import classes from "./Layout.module.css";
+import MenuToggle from "../../components/Navigation/MenuToggle/MenuToggle";
 
 class Layout extends Component {
+
+    state = {
+        menu: false
+    }
+
+    toggleMenuHandler = () => {
+        this.setState(prevState => {
+            return {
+                menu: !prevState.menu
+            };
+        });
+    };
+
     render() {
         return (
             <div className={classes.Layout}>
+
+                <MenuToggle
+                    onToggle={this.toggleMenuHandler}
+                    isOpen={this.state.menu}
+                />
+
                 <main>
                     {this.props.children}
                 </main>
