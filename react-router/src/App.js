ffe1b7b4561f7ce94b84c47e7b8766dbdbdf031d
Vadim Kolchanov <ikolchanov14@gmail.com>
2022-02-26 18:23:51 +0500

    Раздел 7: React Router. Регистрация роута

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,31 +1,41 @@
-import React, {Component} from 'react'
-import './App.scss'
-import About from './About/About'
-import Cars from './Cars/Cars'
+import React, {Component} from 'react';
+import './App.scss';
+import About from './About/About';
+import Cars from './Cars/Cars';
+import {Route, Routes} from 'react-router-dom';
 
 class App extends Component {
-  render() {
+    render() {
 
-    return (
-      <div>
-        <nav className="nav">
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/about">About</a>
-            </li>
-          </ul>
-        </nav>
+        return (
+            <div>
+                <nav className="nav">
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/about">About</a>
+                        </li>
+                    </ul>
+                </nav>
 
-        <hr/>
-        <About />
+                <hr/>
 
-        <Cars />
-      </div>
-    );
-  }
+                {/*localhost:3000*/}
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<h1>Home Page</h1>}
+                    />
+                </Routes>
+
+                <About/>
+
+                <Cars/>
+            </div>
+        );
+    }
 }
 
-export default App
+export default App;
