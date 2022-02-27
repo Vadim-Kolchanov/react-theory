ddafa2e7aab2246bb9cc99e2805ada43aee38004
Vadim Kolchanov <ikolchanov14@gmail.com>
2022-02-27 19:50:53 +0500

    Раздел 7: React Router. Параметры ссылки

--- react-router/src/Cars/Cars.js
+++ react-router/src/Cars/Cars.js
@@ -1,32 +1,34 @@
-import React, {Component} from 'react'
-import Car from './Car/Car'
+import React, {Component} from 'react';
+import Car from './Car/Car';
 
 export default class Cars extends Component {
-  state = {
-    cars: [
-      {name: 'Ford', year: 2018},
-      {name: 'Audi', year: 2016},
-      {name: 'Mazda', year: 2010}
-    ]
-  }
+    state = {
+        cars: [
+            {name: 'Ford', year: 2018},
+            {name: 'Audi', year: 2016},
+            {name: 'Mazda', year: 2010}
+        ]
+    };
 
-  render() {
-    return (
-      <div style={{
-        width: 400,
-        margin: 'auto',
-        paddingTop: '20px'
-      }}>
-        {this.state.cars.map((car, index) => {
-          return (
-            <Car
-              key={index}
-              name={car.name}
-              year={car.year}
-            />
-          )
-        })}
-      </div>
-    )
-  }
+    render() {
+        console.log(this.props);
+
+        return (
+            <div style={{
+                width: 400,
+                margin: 'auto',
+                paddingTop: '20px'
+            }}>
+                {this.state.cars.map((car, index) => {
+                    return (
+                        <Car
+                            key={index}
+                            name={car.name}
+                            year={car.year}
+                        />
+                    );
+                })}
+            </div>
+        );
+    }
 }
