973ac2ffd54622c9583efe569916943647ada1f5
Vadim Kolchanov <ikolchanov14@gmail.com>
2022-03-01 22:54:58 +0500

    Раздел 8: Практика роутинг. Настройка

--- react-quiz/src/index.js
+++ react-quiz/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter, Routes} from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+          <Routes>
+              <App />
+          </Routes>
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
