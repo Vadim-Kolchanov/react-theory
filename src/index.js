c7d0badb79d50b5e31cebb7bef3114338d6a0608
Vadim Kolchanov <ikolchanov14@gmail.com>
2022-03-10 22:05:06 +0500

    Раздел 11: Redux. React и Redux

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App/App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import rootReducer from "./redux/rootReducer";
+
+const store = createStore(rootReducer)
+
+const app = (
+    <Provider store={store}>
+        <App title={'I am from props!'}/>
+    </Provider>
+)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App title={'I am from props!'}/>
+      {app}
   </React.StrictMode>,
   document.getElementById('root')
 );
