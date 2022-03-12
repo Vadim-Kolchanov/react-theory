e7c4b5d3d8a880cdd9b640f4a62a7aa38ddee864
Vadim Kolchanov <ikolchanov14@gmail.com>
2022-03-12 15:13:51 +0500

    Раздел 11: Redux.  Devtools

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App/App';
 import reportWebVitals from './reportWebVitals';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
 import rootReducer from "./redux/rootReducer";
 import reduxThunk from 'redux-thunk'
@@ -24,10 +24,17 @@ const loggerMiddleware = store => next => action => {
     return result;
 };
 
-const store = createStore(rootReducer, applyMiddleware(
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(
     loggerMiddleware,
     reduxThunk
-));
+)));
 
 const app = (
     <Provider store={store}>
