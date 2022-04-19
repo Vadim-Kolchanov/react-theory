231f73eb3ead82ea7b4955f7af47af6f45dbf34f
Vadim Kolchanov <ikolchanov14@gmail.com>
2022-04-19 22:00:57 +0500

    Раздел 13: Теория. React Hooks. Пишем свой хук

--- react-hooks-cc/src/App.js
+++ react-hooks-cc/src/App.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
+function useLogger(value) {
+    useEffect(() => {
+        console.log('Value changed: ', value);
+    }, [value]);
+}
+
+function useInput(initialValue) {
+    const [value, setValue] = useState(initialValue)
+
+    const onChange = event => {
+        setValue(event.target.value)
+    }
+
+    const clear = () => setValue('')
+
+    return {
+        bind: {value, onChange},
+        value,
+        clear
+    }
+}
 
 function App() {
+    const input = useInput('');
+    const lastName = useInput('');
+
+    useLogger(input.value);
+
     return (
         <div className={'container pt-3'}>
+            <input type="text" {...input.bind}/>
+            <input type="text" {...lastName.bind}/>
 
+
+            <button onClick={input.clear}>Очистить</button>
+            <hr/>
+            <h1>{input.value}</h1>
         </div>
     );
 }
