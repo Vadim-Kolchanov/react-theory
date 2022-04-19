302505f402d28a9513afc4c2ea87a10140ec519f
Vadim Kolchanov <ikolchanov14@gmail.com>
2022-04-19 21:38:28 +0500

    Раздел 13: Теория. React Hooks. Добавляем redux c useReducer

--- react-hooks-cc/src/Main.js
+++ react-hooks-cc/src/Main.js
@@ -3,12 +3,12 @@ import {useAlert} from "./Alert/AlertContext";
 
 export default function Main() {
 
-    const {toggle} = useAlert();
+    const {show} = useAlert();
 
     return (
         <>
             <h1>Привет в примере с Context</h1>
-            <button className="btn btn-success" onClick={toggle}>Показать alert</button>
+            <button className="btn btn-success" onClick={() => show('Текст из main.js')}>Показать alert</button>
         </>
     );
 }
