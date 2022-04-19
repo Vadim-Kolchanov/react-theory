d95b65bcfe0f02f41a0a9b22ca973cfe6987a132
Vadim Kolchanov <ikolchanov14@gmail.com>
2022-04-19 21:39:46 +0500

    Раздел 13: Теория. React Hooks. Добавляем redux c useReducer

--- react-hooks-cc/src/App.js
+++ react-hooks-cc/src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import Main from "./Main";
-import Alert from "./Alert/Alert";
-import {AlertProvider} from "./Alert/AlertContext";
 
 function App() {
     return (
-        <AlertProvider>
-            <div className={'container pt-3'}>
-                <Alert />
-                <Main />
-            </div>
-        </AlertProvider>
+        <div className={'container pt-3'}>
+
+        </div>
     );
 }
 
