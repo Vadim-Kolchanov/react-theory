4c43fcd3c57ef9361e070ccd2d1a08e635fe2538
Vadim Kolchanov <ikolchanov14@gmail.com>
2022-04-22 20:29:24 +0500

    Раздел 14: Практика. Создание github state. Урок 2

--- react-hooks-practice/src/App.js
+++ react-hooks-practice/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import {Navbar} from "./components/Navbar";
 import {Home} from "./pages/Home";
 import {About} from "./pages/About";
 import {Profile} from "./pages/Profile";
 import {Alert} from "./components/Alert";
 import {AlertState} from "./context/alert/AlertState";
-import {GithubContext} from "./context/github/GithubContext";
 import {GithubState} from "./context/github/GithubState";
 
 function App() {
--- react-hooks-practice/src/context/github/GithubState.js
+++ react-hooks-practice/src/context/github/GithubState.js
@@ -1,6 +1,7 @@
 import React, {useReducer} from "react";
 import {GithubContext} from "./GithubContext";
 import {GithubReducer} from "./GithubReducer";
+import ActionType from "../action-type";
 
 export const GithubState = ({children}) => {
     const initialState = {
@@ -8,14 +9,55 @@ export const GithubState = ({children}) => {
         users: [],
         loading: false,
         repos: []
-    }
+    };
 
     const [state, dispatch] = useReducer(GithubReducer, initialState);
 
+    const search = async value => {
+        setLoading();
+
+        // ...
+
+        dispatch({
+            type: ActionType.SEARCH_USERS,
+            payload: []
+        });
+    };
+
+    const getUser = async name => {
+        setLoading();
+
+        // ...
+
+        dispatch({
+            type: ActionType.GET_USER,
+            payload: {}
+        });
+    };
+
+    const getRepos = async name => {
+        setLoading();
+
+        // ...
+
+        dispatch({
+            type: ActionType.GET_REPOS,
+            payload: []
+        });
+    };
+
+    const clearUsers = () => dispatch({type: ActionType.CLEAR_USERS});
+
+    const setLoading = () => dispatch({type: ActionType.SET_LOADING});
+
+    const {user, users, repos, loading} = state;
 
     return (
-        <GithubContext.Provider>
+        <GithubContext.Provider value={{
+            setLoading, search, getUser, getRepos, clearUsers,
+            user, users, repos, loading
+        }}>
             {children}
         </GithubContext.Provider>
-    )
-}
+    );
+};
