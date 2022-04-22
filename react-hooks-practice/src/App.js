5156671a40352b0e0bb99b131820db3e7d8dcfe2
Vadim Kolchanov <ikolchanov14@gmail.com>
2022-04-22 19:46:57 +0500

    Раздел 14: Практика. Компонент alert. Урок 1

--- react-hooks-practice/src/App.js
+++ react-hooks-practice/src/App.js
@@ -1,25 +1,30 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {Navbar} from "./components/Navbar";
 import {Home} from "./pages/Home";
 import {About} from "./pages/About";
 import {Profile} from "./pages/Profile";
+import {Alert} from "./components/Alert";
+import {AlertState} from "./context/alert/AlertState";
 
 function App() {
-  return (
-      <BrowserRouter>
-          <Navbar/>
-          <div className="container pt-4">
-              <Routes>
-                  <Route path="/" element={<Home/>}/>
-                  <Route path="/about" element={<About/>}/>
-                  <Route path="/profile/:name" element={<Profile/>}/>
+    return (
+        <AlertState>
+            <BrowserRouter>
+                <Navbar/>
+                <div className="container pt-4">
+                    <Alert alert={{text: 'test alert'}}/>
+                    <Routes>
+                        <Route path="/" element={<Home/>}/>
+                        <Route path="/about" element={<About/>}/>
+                        <Route path="/profile/:name" element={<Profile/>}/>
 
-                  <Route path="*" element={<Home/>}/>
-              </Routes>
-          </div>
-      </BrowserRouter>
-  );
+                        <Route path="*" element={<Home/>}/>
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </AlertState>
+    );
 }
 
 export default App;
